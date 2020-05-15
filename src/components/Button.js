e58991f8d97aadeb5d16c416e02f865bf6db0970
Christian Otieno <oduorchristian@gmail.com>
2020-05-16 02:07:37 +0300

    ADD style to Button and ButtonPanel components

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,15 +3,37 @@ import PropTypes from 'prop-types';
 
 const Button = props => {
   const { name } = props;
+  const { color } = props;
+  const { wide } = props;
+  const { bottomRightRadius } = props;
+  const { bottomLeftRadius } = props;
+  const styles = {
+    flexBasis: wide ? '50%' : '25%',
+    backgroundColor: color,
+    borderBottomRightRadius: bottomRightRadius ? 10 : 0,
+    borderBottomLeftRadius: bottomLeftRadius ? 10 : 0,
+  };
+
   return (
-    <div>
-      <button type="button">
-        {name}
-      </button>
+    <div className="btn" style={styles}>
+      {name}
     </div>
   );
 };
 
-Button.propTypes = { name: PropTypes.string.isRequired };
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
+  bottomLeftRadius: PropTypes.bool,
+  bottomRightRadius: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  color: '#fb8B24',
+  wide: false,
+  bottomLeftRadius: false,
+  bottomRightRadius: false,
+};
 
 export default Button;
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,35 +3,35 @@ import Button from './Button';
 
 const ButtonPanel = () => (
 
-  <div>
-    <div>
+  <div className="btn-panel">
+    <div className="btn-group">
       <Button name="AC" />
       <Button name="+/-" />
       <Button name="%" />
       <Button name="DEL" />
     </div>
-    <div>
+    <div className="btn-group">
       <Button name="7" />
       <Button name="8" />
       <Button name="9" />
       <Button name="X" />
     </div>
-    <div>
+    <div className="btn-group">
       <Button name="4" />
       <Button name="5" />
       <Button name="6" />
       <Button name="-" />
     </div>
-    <div>
+    <div className="btn-group">
       <Button name="1" />
       <Button name="2" />
       <Button name="3" />
       <Button name="+" />
     </div>
-    <div>
-      <Button name="0" />
+    <div className="btn-group">
+      <Button name="0" wide bottomLeftRadius />
       <Button name="." />
-      <Button name="=" />
+      <Button name="=" bottomRightRadius />
     </div>
   </div>
 );
