ef9eeb4e5088e816349f2bf680b6a1dc28a7b5fb
Christian Otieno <oduorchristian@gmail.com>
2020-05-15 21:04:56 +0300

    Implement ButtonPanel component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      result: 0,
+    };
   }
 
   render() {
+    const { result } = this.state; 
     return (
       <div>
-        <Display />
+        <Display result={result.toString()}/>
         <ButtonPanel />
       </div>
     );
