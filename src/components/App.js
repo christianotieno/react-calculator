dc105e4e80856b5c9906149251028382772925a5
Christian Otieno <oduorchristian@gmail.com>
2020-05-16 06:43:26 +0300

    ADD Calculate import in main APP

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Display from './Display';
 import '../styles/css/index.css';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
