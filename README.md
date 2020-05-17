8d554dab215af35df6c4366f97ddb8db5045a7e5
Christian Otieno <oduorchristian@gmail.com>
2020-05-17 18:03:21 +0300

    IMPLEMENT App.js as the stateful parent component of the application

--- README.md
+++ README.md
@@ -84,13 +84,13 @@ To learn React, check out the [React documentation](https://reactjs.org/).
 
 - Github: [@christianotieno](https://github.com/christianotieno)
 - Twitter: [@iamchrisotieno](https://twitter.com/iamchrisotieno)
-- Linkedin: [Christian Otieno](https://linkedin.com/linkedinhandle)
+- Linkedin: [Christian Otieno](https://www.linkedin.com/in/christianotieno/)
 
 ## 🤝 Contributing
 
 Contributions, issues and feature requests are welcome!
 
-Feel free to check the [issues page](https://github.com/christianotieno/todo-list/issues).
+Feel free to check the [issues page](https://github.com/christianotieno/react-calculator/issues).
 
 ## Show your support
 
--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,34 @@ import React from 'react';
 import Display from './Display';
 import '../styles/css/index.css';
 import ButtonPanel from './ButtonPanel';
-// import Calculate from '../logic/calculate';
+import Calculate from '../logic/calculate';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: '0',
+      total: null,
+      next: null,
+      operation: null,
+      current: null,
     };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const state = Calculate(this.state, buttonName);
+    this.setState(state);
   }
 
   render() {
-    const { result } = this.state;
+    const { current } = this.state;
     return (
       <div className="app">
         <div className="calc-container">
-          <Display result={result} />
-          <ButtonPanel />
+          <Display result={current} />
+          <ButtonPanel clickHandler={this.handleClick} />
         </div>
       </div>
     );
